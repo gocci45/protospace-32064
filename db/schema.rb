23d7be72df4e092699630153c0813148df6dcfa0
gotti45 <yuto.yogotti35@icloud.com>
2020-11-18 18:09:52 +0900

    コミット３

--- db/schema.rb
+++ db/schema.rb
@@ -57,15 +57,11 @@ ActiveRecord::Schema.define(version: 2020_11_17_095742) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
-    t.text "profile", null: false
-    t.text "occupation", null: false
-    t.text "position", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
